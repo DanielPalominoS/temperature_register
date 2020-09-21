40347f532a2036394bd872496550d86f10e37e72
DanielPalominoS <daniel.palomino@mail.escuelaing.edu.co>
2020-09-21 15:52:09 -0500

    docker files added

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,11 +23,7 @@ Vue.directive('focus', {
 //Consts
 //const baseURL = "http://localhost:3004"
 //const baseURL = "http://192.168.0.5:3004"
-<<<<<<< HEAD
-const baseURL = "http://192.168.222.150:5001"
-=======
 const baseURL = "http://192.168.222.178:5001"
->>>>>>> test_v2
 //const baseURL = "http://192.168.222.150:5000"
 //
 
