c6e6304838cd51e80c7191bebcc9845f9aa5b836
DanielPalominoS <daniel.palomino@mail.escuelaing.edu.co>
2020-09-21 18:13:05 -0500

    ip_base changed

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,10 +20,7 @@ Vue.directive('focus', {
 })
 
 //Consts
-//const baseURL = "http://localhost:3004"
-//const baseURL = "http://192.168.0.8:3004"
-//const baseURL = "http://192.168.222.150:5000"
-const baseURL = "http://192.168.222.150:5001"
+const baseURL = "http://192.168.222.178:5001"
 //
 
 // Implementing Vue JS
