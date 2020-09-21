57abf48a36fe6025298b08857a7cee914e8f666d
DanielPalominoS <daniel.palomino@mail.escuelaing.edu.co>
2020-09-21 15:06:06 -0500

    script file added

---
+++ docker_tr.sh
@@ -0,0 +1,4 @@
+docker run -it \
+    --name  temp-logs \
+    -d \
+    -p 8081:80 danielspsp/milleregistrotemperatura:latest
--- resources/js/app.js
+++ resources/js/app.js
@@ -23,7 +23,7 @@ Vue.directive('focus', {
 //Consts
 //const baseURL = "http://localhost:3004"
 //const baseURL = "http://192.168.0.5:3004"
-const baseURL = "http://192.168.222.150:5001"
+const baseURL = "http://192.168.222.178:5001"
 //const baseURL = "http://192.168.222.150:5000"
 //
 
